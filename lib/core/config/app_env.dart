01dbbb86f1a3dedee2dfaefb0b690c461b5ec22f
Jonathan Thiébaud <jonathan.thiebaud@hes-so.ch>
2025-09-04 11:23:13 +0200

    feat : edit capture

--- lib/core/config/app_env.dart
+++ lib/core/config/app_env.dart
@@ -1,6 +1,5 @@
 /// Compile-time env (via --dart-define)
 class AppEnv {
-  /// TODO : set API URL
   static const apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
     defaultValue: 'http://10.0.2.2:8000', // Default emulator value
